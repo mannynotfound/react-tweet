c3b6fb4186f7e04032e34cde5f02d32a95ca2abc
art1fa <art1fa@mailbox.org>
2018-01-10 18:17:10 +0100

    Update to current Tweet design

--- src/components/Tweet/Header.js
+++ src/components/Tweet/Header.js
@@ -3,6 +3,15 @@ import PropTypes from 'prop-types'
 import styles from './styles'
 import twemoji from 'twemoji'
 
+function VerifiedBadge() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="12.6" viewBox="0 0 24 24" className="htc-icon htc-icon--verified">
+      <path style={{fill: "#1DA1F2"}} opacity="0" d="M0 0h24v24H0z"></path> 
+      <path style={{fill: "#1DA1F2"}} d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5c-1.51 0-2.816.917-3.437 2.25-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z"></path> 
+    </svg>
+  );
+}
+
 class Header extends React.Component {
   constructor(props) {
     super(props)
@@ -38,6 +47,12 @@ class Header extends React.Component {
   render () {
     const {data, linkProps} = this.props
     const timestamp = this.createTimestamp(data.created_at)
+    
+    let verified = null;
+    if (data.user.verified) {
+      verified = <VerifiedBadge />;
+    }
+    
 
     let {name} = data.user
     name = twemoji.parse(name)
@@ -45,17 +60,18 @@ class Header extends React.Component {
 
     return (
       <div className="header">
-        <a className="account-group" style={styles.accountGroup} href={`http://twitter.com/${data.user.screen_name}`} {...linkProps}>
+        <a className="account-group" style={styles.accountGroup} href={`https://twitter.com/${data.user.screen_name}`} {...linkProps}>
           <img className="avatar" src={data.user.profile_image_url} style={styles.avatar} />
-          <strong className="fullname" style={styles.fullname}>{data.user.name}{' '}</strong>
-          <span>&rlm;</span>
+          <strong className="fullname" style={styles.fullname}>{data.user.name}</strong>
+          {verified}
+          <span>&nbsp;</span>
           <span className="username" style={styles.username}>
             <s style={styles.s}>{'@'}</s>
             <b style={styles.b}>{data.user.screen_name}</b>
           </span>
         </a>
         <small className="time" style={styles.time}>
-          <a href={`http://twitter.com/${data.user.screen_name}/status/${data.id_str}`} className="tweet-timestamp" style={styles.timestamp} {...linkProps}>
+          <a href={`https://twitter.com/${data.user.screen_name}/status/${data.id_str}`} className="tweet-timestamp" style={styles.timestamp} {...linkProps}>
             {' • '}{timestamp}
           </a>
         </small>
