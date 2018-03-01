57b17994317688b718570d9ae0828dafe2119369
mohitmutha <mohit.mutha@siemens.com>
2018-03-01 10:34:00 +0530

    Add gif | video condition

--- src/components/Tweet/Video.js
+++ src/components/Tweet/Video.js
@@ -5,7 +5,7 @@ import VideoJS from 'react-videojs'
 
 class Video extends React.Component {
   render () {
-    let {media, gif} = this.props, videoSrc = ''
+    let {media, gif, autoPlay} = this.props, videoSrc = ''
 
     media[0].video_info.variants.forEach( v => {
       if (v.url.indexOf('.mp4') > -1) {
@@ -14,14 +14,14 @@ class Video extends React.Component {
     })
 
     let VideoComponent = (
-      <video src={videoSrc} controls={!gif} autoPlay={this.props.autoPlay} loop={gif} style={styles.video}>
+      <video src={videoSrc} controls={!gif} autoPlay={gif || autoPlay} loop={gif} style={styles.video}>
         {'Your browser does not support the '}<code>{'video '}</code>{'element.'}
       </video>
     )
 
     if (typeof videojs !== 'undefined') {
       VideoComponent = (
-        <VideoJS src={videoSrc} controls={!gif} autoPlay={this.props.autoPlay} loop={gif} style={styles.video}>
+        <VideoJS src={videoSrc} controls={!gif} autoPlay={gif || autoPlay} loop={gif} style={styles.video}>
           {'Your browser does not support the '}<code>{'video '}</code>{'element.'}
         </VideoJS>
       )
