83a6630d62193315c988e452642e84e43d752962
mannynotfound <manuel.palou@buzzfeed.com>
2018-03-22 13:05:55 -0400

    use react-video-wrapper instead of react-videojs

--- src/components/Tweet/Video.js
+++ src/components/Tweet/Video.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles'
-import VideoJS from 'react-videojs'
+import VideoJS from 'react-video-wrapper'
 
 class Video extends React.Component {
   render () {
