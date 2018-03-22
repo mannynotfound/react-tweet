567a6d3adc32afdd57f476dc7a7fad5ec79e3908
Sudarshan Gaikaiwari <sudarshan@acm.org>
2018-03-22 15:06:21 -0500

    Filtered Tweets to a single video tweet

--- example/app.js
+++ example/app.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Tweet from '../src/components/Tweet/Tweet.js';
 import './_app.css';
-import tweets from './tweets'
+import tweets from './tweets';
 
 const linkProps = { target: '_blank' };
 
-ReactDom.render((
-  <div className="ExamplePage" style={{'width': '590px', 'margin': '0 auto'}}>
-    <div className="tweet-stream" style={{'width': '100%'}}>
-      {tweets.map((t, i) => (
-        <Tweet autoPlay={true} data={t} key={i} linkProps={linkProps} />
-      ))}
+const filteredTweets = tweets.filter(tweet => {
+  if (tweet.id === 953583426153713665) {
+    return tweet;
+  }
+  return null;
+});
+
+ReactDom.render(
+  <div className="ExamplePage" style={{ width: '590px', margin: '0 auto' }}>
+    <div className="tweet-stream" style={{ width: '100%' }}>
+      {filteredTweets.map((t, i) => <Tweet autoPlay data={t} key={i} linkProps={linkProps} />)}
     </div>
-  </div>
-), document.getElementById('container'))
+  </div>,
+  document.getElementById('container')
+);
